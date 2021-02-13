3db60c7d039c0e334a784bf7142da0443e928642
Jon Jelinek <jjofnwa@gmail.com>
2021-02-13 00:36:15 -0600

    Solved problem 54 - Poker game winner

--- problems/54.py
+++ problems/54.py
@@ -24,7 +24,7 @@ def straight(v):
     sorted_values = sorted(v)
     consecutive_list = list(range(min(v), max(v) + 1))
     if sorted_values == consecutive_list:
-        return True
+        return True, max(v)
     return False
 
 
@@ -37,12 +37,15 @@ def flush(counts):
 
 def straight_flush(v,suits):
     if straight(v) and flush(Counter(suits)):
-        return True
+        return True, max(v)
     return False
 
 
 def royal_flush(v, suits):
-    if straight_flush(v, suits):
+    result = straight_flush(v, suits)
+    if not result:
+        return False
+    if result[0]:
         if max(v) == 14:
             return True
     return False
@@ -51,42 +54,49 @@ def royal_flush(v, suits):
 def four_kind(counts):
     for key in counts:
         if counts[key] >= 4:
-            return True
+            return True, key
     return False
 
 
 def three_kind(counts):
     for key in counts:
         if counts[key] >= 3:
-            return True
+            return True, key
     return False
 
 
 def pair(counts):
-    for key in counts:
+    for key in sorted(counts, reverse=True):
         if counts[key] >= 2:
-            return True
+            return True, key
     return False
 
 
 def two_pair(counts):
     pairs = 0
+    kvalue = 0
     for key in counts:
         if counts[key] >= 4:
             pairs += 2
+            kvalue = key
         else:
             if counts[key] >= 2:
                 pairs += 1
+                if key > kvalue:
+                    kvalue = key
     if pairs >= 2:
-        return True
+        return True, kvalue
     return False
 
 
 def full_house(counts):
-    if three_kind(counts):
+    result = three_kind(counts)
+    if not result:
+        return False
+    if result[0]:
         for key in counts:
             if counts[key] == 2:
-                return True
+                return True, result[1]
     return False
 
 
@@ -136,7 +146,35 @@ def get_poker_score(values, suits):
     if pair(values_counts):
         return op
 
-    return 0    # high_card(values)
+    return high_card(values)
+
+
+def get_finer_poker_score(values, suits):
+    values_counts = Counter(values)
+
+    result = straight_flush(values, suits)
+    if result is not False and result[0]:
+        return result[1]
+    result = four_kind(values_counts)
+    if result is not False and result[0]:
+        return result[1]
+    result = full_house(values_counts)
+    if result is not False and result[0]:
+        return result[1]
+    result = straight(values)
+    if result is not False and result[0]:
+        return result[1]
+    result = three_kind(values_counts)
+    if result is not False and result[0]:
+        return result[1]
+    result = two_pair(values_counts)
+    if result is not False and result[0]:
+        return result[1]
+    result = pair(values_counts)
+    if result is not False and result[0]:
+        return result[1]
+
+    return 0
 
 
 def main():
@@ -154,15 +192,42 @@ def main():
         if p1_score > p2_score:
             p1_wins += 1
         else:
-            if p2_score > p1_score:
+            if p1_score < p2_score:
                 p2_wins += 1
             else:
-                # Identical score, hand with highest sum wins
-                if sum(p1_values) > sum(p2_values):
-                    p1_wins += 1
-                else:
-                    p2_wins += 1
-    print("Player 1 won {} games, and Player 2 won {} games.".format(p1_wins, p2_wins))
+                if p1_score == p2_score and p1_score > 14:
+                    # data set seems to only get here on 15's, meaning one pair conflicts
+                    p1_finer_score = get_finer_poker_score(p1_values, p1_suits)
+                    p2_finer_score = get_finer_poker_score(p2_values, p2_suits)
+                    if p1_finer_score > p2_finer_score:
+                        p1_wins += 1
+                    else:
+                        if p1_finer_score < p2_finer_score:
+                            p2_wins += 1
+                        else:
+                            # finer scores match, meaning this needs to be determined by high_card, setting scores to 0, so next section will determine winner
+                            p1_score = 0
+                            p2_score = 0
+                            # print("Error: should not be possible to get here.  score {} p1_cards {} p2_cards {}".format(p1_score, p1_cards, p2_cards))
+                if p1_score == p2_score and p1_score <= 14:
+                    # Identical score, unique high card wins
+                    # Remove matching high cards from each hand
+                    # print("High card P1 wins? {}".format(high_card(p1_values) > high_card(p2_values)))
+                    while high_card(p1_values) == high_card(p2_values):
+                        # Possible error here for empty list
+                        # print("Removing {}".format(high_card(p1_values)))
+                        p1_values.remove(high_card(p1_values))
+                        p2_values.remove(high_card(p2_values))
+
+                    if high_card(p1_values) > high_card(p2_values):
+                        p1_wins += 1
+                    else:
+                        if high_card(p1_values) < high_card(p2_values):
+                            p2_wins += 1
+                        else:
+                            print("ERROR: Unable to determine winner for game {}".format(line))
+
+    print("Player 1 won {} games, and Player 2 won {} games out of {} total games".format(p1_wins, p2_wins, p1_wins + p2_wins))
 
 
 assert get_card_value('T') == 10
@@ -171,33 +236,44 @@ assert get_card_value('Q') == 12
 assert get_card_value('K') == 13
 assert get_card_value('A') == 14
 
-assert four_kind(Counter([10, 10, 10, 13, 10])) is True
+assert four_kind(Counter([10, 10, 10, 13, 10]))[0] is True
+assert four_kind(Counter([10, 10, 10, 13, 10]))[1] == 10
 assert four_kind(Counter([5, 12, 3, 13, 10])) is False
 
-assert three_kind(Counter([10, 10, 10, 13, 10])) is True
+assert three_kind(Counter([10, 10, 10, 13, 10]))[0] is True
+assert three_kind(Counter([10, 10, 10, 13, 10]))[1] == 10
 assert three_kind(Counter([5, 12, 3, 13, 10])) is False
 
-assert pair(Counter([10, 10, 10, 13, 10])) is True
+assert pair(Counter([10, 10, 10, 13, 10]))[0] is True
+assert pair(Counter([10, 10, 10, 13, 10]))[1] == 10
+assert pair(Counter([2, 2, 7, 7, 10]))[1] == 7
 assert pair(Counter([5, 12, 3, 13, 10])) is False
 
-assert two_pair(Counter([10, 10, 10, 13, 10])) is True
+assert two_pair(Counter([10, 10, 10, 13, 10]))[0] is True
+assert two_pair(Counter([10, 10, 10, 13, 10]))[1] == 10
 assert two_pair(Counter([5, 12, 3, 13, 10])) is False
 
-assert full_house(Counter([10, 5, 10, 5, 10])) is True
+assert full_house(Counter([10, 5, 10, 5, 10]))[0] is True
+assert full_house(Counter([10, 5, 10, 5, 10]))[1] == 10
 assert full_house(Counter([2, 5, 10, 5, 10])) is False
 assert full_house(Counter([5, 5, 5, 5, 10])) is False
 
 assert flush(Counter(['D', 'D', 'D', 'D', 'D'])) is True
 assert flush(Counter(['D', 'D', 'H', 'D', 'D'])) is False
 
-assert straight([2, 3, 4, 5, 6]) is True
-assert straight([8, 9, 10, 11, 12]) is True
+assert straight([2, 3, 4, 5, 6])[0] is True
+assert straight([2, 3, 4, 5, 6])[1] == 6
+assert straight([8, 9, 10, 11, 12])[0] is True
+assert straight([8, 9, 10, 11, 12])[1] == 12
 assert straight([4, 9, 10, 11, 12]) is False
 
-assert straight_flush([2, 3, 4, 5, 6], ['D', 'D', 'D', 'D', 'D']) is True
-assert straight_flush([8, 9, 10, 11, 12], ['C', 'C', 'C', 'C', 'C']) is True
+assert straight_flush([2, 3, 4, 5, 6], ['D', 'D', 'D', 'D', 'D'])[0] is True
+assert straight_flush([2, 3, 4, 5, 6], ['D', 'D', 'D', 'D', 'D'])[1] == 6
+assert straight_flush([8, 9, 10, 11, 12], ['C', 'C', 'C', 'C', 'C'])[0] is True
+assert straight_flush([8, 9, 10, 11, 12], ['C', 'C', 'C', 'C', 'C'])[1] == 12
 assert straight_flush([8, 9, 10, 11, 12], ['C', 'H', 'C', 'D', 'C']) is False
-assert straight_flush([10, 11, 12, 13, 14], ['D', 'D', 'D', 'D', 'D']) is True
+assert straight_flush([10, 11, 12, 13, 14], ['D', 'D', 'D', 'D', 'D'])[0] is True
+assert straight_flush([10, 11, 12, 13, 14], ['D', 'D', 'D', 'D', 'D'])[1] == 14
 
 assert royal_flush([10, 11, 12, 13, 14], ['D', 'D', 'D', 'D', 'D']) is True
 assert royal_flush([10, 11, 12, 13, 14], ['D', 'C', 'D', 'D', 'D']) is False
@@ -205,6 +281,14 @@ assert royal_flush([10, 11, 12, 13, 14], ['D', 'C', 'D', 'D', 'D']) is False
 assert high_card([10, 11, 12, 13, 14]) == 14
 assert high_card([2, 2, 2, 4, 2]) == 4
 
-main()
+assert high_card([6, 7, 8, 12, 13]) == high_card([5, 6, 7, 8, 13])
 
-# wrong answers: 375
+v1 = [2, 8, 8, 8, 8]
+v2 = [3, 8, 8, 8, 8]
+while high_card(v1) == high_card(v2):
+    v1.remove(high_card(v1))
+    v2.remove(high_card(v2))
+assert high_card(v1) < high_card(v2)
+
+
+main()
